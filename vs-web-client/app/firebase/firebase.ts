a0fec34ef0027f3a45f8506cc658235e5a6cf324
ricebiceps <ricebiceps@gmail.com>
2024-08-06 20:04:15 -0400

    added google signin using firebase to the web app ui

---
+++ vs-web-client/app/firebase/firebase.ts
@@ -0,0 +1,49 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+import { 
+    getAuth, 
+    signInWithPopup,
+    GoogleAuthProvider,
+    onAuthStateChanged,
+    User
+} from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA2pv0vTLNBibZ6ISPhSjgiIW3MTDtp_VM",
+  authDomain: "video-skeleton.firebaseapp.com",
+  projectId: "video-skeleton",
+  appId: "1:851867535388:web:77d5487d17e48b730f77d9",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+/**
+ * Signs the user in with a Google popup.
+ * @reutrns a promise that resolves with the user's credentials
+ */
+export function signInWithGoogle(){
+    return signInWithPopup(auth, new GoogleAuthProvider());
+}
+
+/**
+ * Signs the user out.
+ * @returns a promise that resolves when the user is signed out
+ */
+export function signOut() {
+    return auth.signOut();
+}
+
+/**
+ * Triggers a callback when user auth state changes.
+ * @returns A function to unsubscribe callback
+ */
+export function onAuthStateChangedHelper(callback: (user: User | null) => void){
+    return onAuthStateChanged(auth, callback);
+}
--- vs-web-client/app/navbar/navbar.tsx
+++ vs-web-client/app/navbar/navbar.tsx
@@ -1,15 +1,33 @@
+'use client';
+
 import Image from "next/image";
 import Link from "next/link";
 
 import styles from "./navbar.module.css";
+import SignIn from "./sign-in";
+import { onAuthStateChangedHelper } from "../firebase/firebase";
+import { useEffect, useState } from "react";
+import { User } from "firebase/auth";
 
-export default function Navbar(){
-    return (
-        <nav className={styles.nav}>
-            <Link href="/">
-                <Image width={30} height={40} 
-                src="/ImperialRoundY.svg" alt="Y Logo"/>
-            </Link>
-        </nav>
-    );
+export default function Navbar() {
+  // init user state
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedHelper((user) => {
+      setUser(user);
+    });
+    // Cleanup subscription on unmount
+    return () => unsubscribe();
+  });
+
+  return (
+    <nav className={styles.nav}>
+      <Link href="/">
+        <Image width={40} height={50}
+          src="/ImperialRoundY.svg" alt="Y Logo"/>
+      </Link>
+      <SignIn user={user} />
+    </nav>
+  );
 }
