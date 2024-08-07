45247f8752bf7ac6247ee686200635982fa4fbf4
ricebiceps <ricebiceps@gmail.com>
2024-08-07 12:53:19 -0400

    implemented and deployed the function that generates an upload URL for end users

--- vs-api-service/functions/src/index.ts
+++ vs-api-service/functions/src/index.ts
@@ -2,10 +2,16 @@ import * as functions from "firebase-functions";
 import {Firestore} from "firebase-admin/firestore";
 import * as logger from "firebase-functions/logger";
 import * as admin from "firebase-admin";
+import {Storage} from "@google-cloud/storage";
+import {onCall} from "firebase-functions/v2/https";
 
 admin.initializeApp();
 
 const firestore = new Firestore();
+const storage = new Storage();
+
+
+const rawVideoBucketName = "service-raw-videos";
 
 export const createUser = functions.auth.user().onCreate((user) => {
   const userInfo = {
@@ -19,3 +25,27 @@ export const createUser = functions.auth.user().onCreate((user) => {
   return;
 });
 
+export const generateUploadUrl = onCall({maxInstances: 1}, async (request) => {
+  // check if user is authenticated
+  if (!request.auth) {
+    throw new functions.https.HttpsError(
+      "failed-precondition",
+      "The function must be called while authenticated"
+    );
+  }
+  const auth = request.auth;
+  const data = request.data;
+  const bucket = storage.bucket(rawVideoBucketName);
+
+  // Generate a unique filename for upload
+  const fileName = `${auth.uid}-${Date.now()}.${data.fileExtension}`;
+
+  // Get a v4 signed URL for uploading file
+  const [url] = await bucket.file(fileName).getSignedUrl({
+    version: "v4",
+    action: "write",
+    expires: Date.now() + 15 * 60 * 1000, // 15 minutes
+  });
+
+  return {url, fileName};
+});
